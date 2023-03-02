4984807718b6f3e7b059d70825cf2e844f1d057c
DCoppee <dominiquecoppee.pro@gmail.com>
2023-03-02 16:53:44 +0100

    vite + components + api start

---
+++ moviesapp-vite/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+import RandomMv from './RandomMv';
+
+const App = () => {
+  return (
+    <div>
+      <Nav /><RandomMv />
+    </div>
+  );
+};
+
+export default App;
---
+++ moviesapp-vite/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            <div className='logo-menu'></div>
+            <input className='input-search' type="text" placeholder="Search"></input>
+        </div>
+    );
+};
+
+export default Nav;
